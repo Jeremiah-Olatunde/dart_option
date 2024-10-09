83a33d9c8ad6cfe5e822113224487135b6b6a222
Jeremiah Olatunde <jesuseun.j.olatunde@gmail.com>
2024-10-09 21:15:09 +0100

    chore: remove statement from bin/dart_option file

--- bin/dart_option.dart
+++ bin/dart_option.dart
@@ -3,6 +3,4 @@ import 'package:dart_option/dart_option.dart';
 void main(List<String> arguments) {
   print('Dart Option');
   print('-------------------------------------');
-
-  print(iter(Some(10)));
 }
