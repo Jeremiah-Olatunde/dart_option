20f6c40ac24263c4b428964617f8384d5d04c342
Jeremiah Olatunde <jesuseun.j.olatunde@gmail.com>
2024-10-09 21:22:39 +0100

    feat(lib): add function isSomeAnd

--- lib/dart_option.dart
+++ lib/dart_option.dart
@@ -11,6 +11,13 @@ bool isSome<T>(Option<T> option) {
   return switch (option) { Some(value: _) => true, None() => false };
 }
 
+bool isSomeAnd<T>(Option<T> option, bool Function(T) predicate) {
+  return switch (option) {
+    Some(value: T value) => predicate(value),
+    None() => false,
+  };
+}
+
 Option<U> map<T, U>(Option<T> option, U Function(T) f) {
   return switch (option) {
     Some(value: T value) => Some(f(value)),
